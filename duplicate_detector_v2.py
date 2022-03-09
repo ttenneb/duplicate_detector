5f7d2d0c28f76edfe1daa8ec5677780b2062ab5e
ttenneb <bcg77@scarletmail.rutgers.edu>
2022-03-09 01:08:01 -0500

    ramsey conversion w/ model and data

--- duplicate_detector_v2.py
+++ duplicate_detector_v2.py
@@ -16,6 +16,8 @@ from sklearn.model_selection import learning_curve, train_test_split
 from sklearn.ensemble import StackingRegressor, HistGradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
 
+import pickle
+
 data_train = pd.read_csv(open("data.csv"))
 selected_features = ["seconds", "trajectory_distance", "ssim", "ln_trajectory_distance", "start_distance",
                      "ln_start_distance", 'coefficient1', 'intercept1', 'coefficient2', 'intercept2',
@@ -37,11 +39,6 @@ y = data_train["duplicate"]
 
 x_train, x_val, y_train, y_val = train_test_split(x, y, stratify=y, test_size=.2, random_state=0)
 
-# models = []
-# for i in range(5):
-#     models.append(("RF" + str(i), RandomForestRegressor(n_estimators=150, random_state=22)))
-#
-# clf = StackingRegressor(estimators=models, final_estimator=svm.SVR())
 clf = RandomForestRegressor(n_estimators=150, random_state=22)
 
 clf.fit(x_train.values, y_train)
@@ -62,18 +59,20 @@ plt.plot(
     lw=lw,
 )
 plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
-plt.xlim([0.0, .1])
+plt.xlim([0.0, 1.05])
 plt.ylim([0.0, 1.05])
 plt.xlabel("False Positive Rate")
 plt.ylabel("True Positive Rate")
 plt.title("Receiver operating characteristic example")
 plt.legend(loc="lower right")
+
 plt.show()
 
 min = 1
 save = -1
 row = 0
 opt = thresholds[np.argmin(abs(tpr - (1 - fpr)))]
+
 print("thresh: ", opt)
 
 y_pred_labels = []
@@ -88,21 +87,6 @@ for i in y_val:
         y_val_labels.append("T")
     else:
         y_val_labels.append("F")
-print(accuracy_score(y_val_labels, y_pred_labels))
 print(classification_report(y_val_labels, y_pred_labels))
 print(confusion_matrix(y_val_labels, y_pred_labels))
-y_test = clf.predict(x_train)
-
-fpr, tpr, thresholds = metrics.roc_curve(y_train.astype(int), y_test, pos_label=1)
-opt = thresholds[np.argmin(abs(tpr - (1 - fpr)))]
-
-print("thresh:", opt)
-
-total = 0
-for i, row in data_train.iterrows():
-    input = [data_train.iloc[i, data_train.columns.get_indexer(selected_features)].values]
-    x = clf.predict(input)
-    if x > opt and data_train.at[i, "duplicate"] == 0:
-        print(data_train.iloc[i, 0] + ", " + data_train.iloc[i, 1])
-        total += 1
-print(total / len(data_train.index))
+pickle.dump(clf, open("decision_tree_ramsey.sav", 'wb'))
--- generate_ramsey.py
+++ generate_ramsey.py
@@ -8,28 +8,24 @@ import ast
 import math
 import numpy as np
 import pandas as pd
+import time
+
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 def getfile(args):
     path = args.data_path
     data = pd.read_csv(path)
     data['start_time'] = pd.to_datetime(data['start_time'])
     data['end_time'] = pd.to_datetime(data['end_time'])
+    print(data.shape)
+    data = data[(data["Validation"] == True) | (data["Explanation"] == 'Duplicate')]
+    print(data.shape)
     return data
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--data-path', type=str, default="Ramsey.xlsx", help='what data file to read')
-
-    args = parser.parse_args()
-
-data = getfile(args)
-
-last = None
-saved = None
-dataset_dup = []
-dataset_not_dup = []
 
 
 def trajectoy_distance(list1, list2):
@@ -62,101 +58,126 @@ def predict_path(list):
     return [reg.coef_, reg.intercept_]
 
 
-is_last_duplicate = False
-duplicate_count = 0
-dataset = []
-for index, row in data.iterrows():
+def create_dataset(data):
+    last = None
+    saved = None
+    dataset_dup = []
+    dataset_not_dup = []
+    is_last_duplicate = False
+    duplicate_count = 0
+    dataset = []
+    for index, row in data.iterrows():
 
-    # print(row)
-    if row['Explanation'] == 'Duplicate':
-        temp = last
-        last = row
+        # print(row)
+        if row['Explanation'] == 'Duplicate':
+            temp = last
+            last = row
 
-        dist, start_dist = trajectoy_distance(temp["trajectory"], last["trajectory"])
-
-        coefficient1, intercept1 = predict_path(temp["trajectory"])
-        coefficient2, intercept2 = predict_path(last["trajectory"])
-
-        time_dist = last["start_time"] - temp["end_time"]
-        time_dist = time_dist.total_seconds()
-
-        if is_last_duplicate:
-            dataset.append((temp["clip"], last["clip"], dist, math.log(dist), start_dist, math.log(start_dist), time_dist,
-                            coefficient1[0][0], intercept1[0], coefficient2[0][0], intercept2[0], 1, duplicate_count, "T"))
-        else:
-            dataset.append((temp["clip"], last["clip"], dist, math.log(dist), start_dist, math.log(start_dist), time_dist,
-                            coefficient1[0][0], intercept1[0], coefficient2[0][0], intercept2[0], 0, duplicate_count, "T"))
-
-        is_last_duplicate = True
-        duplicate_count += 1
-    elif row[8] != "clip":
-
-        temp = last
-        last = row
-        if temp is not None:
             dist, start_dist = trajectoy_distance(temp["trajectory"], last["trajectory"])
 
             coefficient1, intercept1 = predict_path(temp["trajectory"])
             coefficient2, intercept2 = predict_path(last["trajectory"])
 
-            time_dist = last["start_time"] - temp['end_time']
+            time_dist = last["start_time"] - temp["end_time"]
             time_dist = time_dist.total_seconds()
 
             if is_last_duplicate:
                 dataset.append((temp["clip"], last["clip"], dist, math.log(dist), start_dist, math.log(start_dist), time_dist,
-                                coefficient1[0][0], intercept1[0], coefficient2[0][0], intercept2[0], 1, duplicate_count, "F"))
+                                coefficient1[0][0], intercept1[0], coefficient2[0][0], intercept2[0], 1, duplicate_count, "T"))
             else:
                 dataset.append((temp["clip"], last["clip"], dist, math.log(dist), start_dist, math.log(start_dist), time_dist,
-                                coefficient1[0][0], intercept1[0], coefficient2[0][0], intercept2[0], 0, duplicate_count, "F"))
-        is_last_duplicate = False
-        duplicate_count = 0
+                                coefficient1[0][0], intercept1[0], coefficient2[0][0], intercept2[0], 0, duplicate_count, "T"))
 
-size = 100
-output = []
-accuracy_total = 0
-total_dup = 0
-total_not_dup = 0
-true_total_dup = 0
-true_total_not_dup = 0
-table = [[0, 0], [0, 0]]
-for i in range(len(dataset)):
-    cap1 = cv.VideoCapture(dataset[i][0])
-    last_frame_num = cap1.get(cv.CAP_PROP_FRAME_COUNT)
-    cap1.set(cv.CAP_PROP_POS_FRAMES, last_frame_num - 3)
-    cap2 = cv.VideoCapture(dataset[i][1])
-    print(100 * (i / float(len(dataset))))
-    total = 0
-    count = 0
-    while cap1.isOpened() and cap2.isOpened():
-        ret1, frame1 = cap1.read()
-        ret2, frame2 = cap2.read()
+            is_last_duplicate = True
+            duplicate_count += 1
+        elif row[8] != "clip":
 
-        cv.imwrite("test.png", frame1)
-        # ret2, frame3 = cap2.read()
-        # ret2, frame4 = cap2.read()
-        if frame1 is not None and frame2 is not None:
-            frame1 = frame1[10:581, 268:574]
-            frame2 = frame2[10:581, 268:574]
-            # frame3 = frame1[25:1530, 635:]
-            # frame4 = frame2[25:1530, 635:]
-            dist1 = sm.structural_similarity(frame1, frame2, multichannel=True)
-            # dist2 = sm.structural_similarity(frame1, frame3, multichannel=True)
-            # dist3 = sm.structural_similarity(frame1, frame4, multichannel=True)
-            # print(dist, dataset[i][2], dataset[i][3], dataset[i][4])
-            dataset[i] += (dist1,)
-            print(dataset[i])
-        count += 1
-        if count > 0:
-            break
-with open('data.csv', 'w', newline='') as csvfile:
-    csvfile.truncate(0)
+            temp = last
+            last = row
+            if temp is not None:
+                dist, start_dist = trajectoy_distance(temp["trajectory"], last["trajectory"])
 
-    output = csv.writer(csvfile)
-    output.writerow(
-        ['video1', 'video2', "trajectory_distance", "ln_trajectory_distance", "start_distance", "ln_start_distance",
-         'seconds', 'coefficient1', 'intercept1', 'coefficient2', 'intercept2', 'is_last_duplicate', 'duplicate_count',
-         'duplicate', 'ssim'])
-    for row in dataset:
-        print(row)
-        output.writerow(row)
+                coefficient1, intercept1 = predict_path(temp["trajectory"])
+                coefficient2, intercept2 = predict_path(last["trajectory"])
 
+                time_dist = last["start_time"] - temp['end_time']
+                time_dist = time_dist.total_seconds()
+
+                if is_last_duplicate:
+                    dataset.append((temp["clip"], last["clip"], dist, math.log(dist), start_dist, math.log(start_dist), time_dist,
+                                    coefficient1[0][0], intercept1[0], coefficient2[0][0], intercept2[0], 1, duplicate_count, "F"))
+                else:
+                    dataset.append((temp["clip"], last["clip"], dist, math.log(dist), start_dist, math.log(start_dist), time_dist,
+                                    coefficient1[0][0], intercept1[0], coefficient2[0][0], intercept2[0], 0, duplicate_count, "F"))
+            is_last_duplicate = False
+            duplicate_count = 0
+    return dataset
+
+def crop(img):
+    return img[10:581, 268:574]
+
+def add_image_features(dataset):
+    t = Timer()
+    t.start()
+    size = 100
+    accuracy_total = 0
+    total_dup = 0
+    total_not_dup = 0
+    true_total_dup = 0
+    true_total_not_dup = 0
+    table = [[0, 0], [0, 0]]
+
+    for i in range(len(dataset)):
+        cap1 = cv.VideoCapture(dataset[i][0])
+        last_frame_num = cap1.get(cv.CAP_PROP_FRAME_COUNT)
+        cap1.set(cv.CAP_PROP_POS_FRAMES, last_frame_num - 3)
+        cap2 = cv.VideoCapture(dataset[i][1])
+        print(100 * (i / float(len(dataset))))
+        total = 0
+        count = 0
+        while cap1.isOpened() and cap2.isOpened():
+            ret1, frame1 = cap1.read()
+            ret2, frame2 = cap2.read()
+
+            # ret2, frame3 = cap2.read()
+            # ret2, frame4 = cap2.read()
+            if frame1 is not None and frame2 is not None:
+                frame1 = crop(frame1)
+                frame2 = crop(frame2)
+                # frame3 = frame1[25:1530, 635:]
+                # frame4 = frame2[25:1530, 635:]
+                dist1 = sm.structural_similarity(frame1, frame2, multichannel=True)
+                # dist2 = sm.structural_similarity(frame1, frame3, multichannel=True)
+                # dist3 = sm.structural_similarity(frame1, frame4, multichannel=True)
+                # print(dist, dataset[i][2], dataset[i][3], dataset[i][4])
+
+                dataset[i] += (dist1,)
+            count += 1
+            if count > 0:
+                break
+    t.stop()
+    return dataset
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data-path', type=str, default="", help='csv path')
+
+    args = parser.parse_args()
+
+    trespassing_data = getfile(args)
+    ramsey_gen = create_dataset(trespassing_data[:20])
+    ramsey_gen = add_image_features(ramsey_gen)
+
+    with open('data.csv', 'w', newline='') as csvfile:
+        csvfile.truncate(0)
+
+        output = csv.writer(csvfile)
+        output.writerow(
+            ['video1', 'video2', "trajectory_distance", "ln_trajectory_distance", "start_distance", "ln_start_distance",
+             'seconds', 'coefficient1', 'intercept1', 'coefficient2', 'intercept2', 'is_last_duplicate',
+             'duplicate_count',
+             'duplicate', 'ssim'])
+        for row in ramsey_gen:
+            output.writerow(row)
